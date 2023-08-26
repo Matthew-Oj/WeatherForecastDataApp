065febcae77f827fbbfd1d2ffec6419482d67aa4
Matthew <edafeoj@gmail.com>
2023-08-26 13:48:03 +0100

    Created a backend and filtered the data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forcast for the Next Day")
 
@@ -14,15 +15,8 @@ st.subheader(f"{option} for the next {days} days in {place}")
 
 
 # add this to a function and import
-def get_data(days):
-    # arrays have the match to work
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
 
-
-d, t = get_data(days)
+data = get_data()
 
 # gets a figure object from a plotting library
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
